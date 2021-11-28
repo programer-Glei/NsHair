a0a28129395f4865151cec5e9a0ccb43e2aa6167
Gleibson Pereira <gleibson.santos05@gmail.com>
2021-11-28 20:00:01 -0300

    proximo comit

--- javascript/script.js
+++ javascript/script.js
@@ -15,6 +15,29 @@ function mudarT(){
 
 mudarT()
 
+window.onscroll = () =>{
+    
+    if(window.scrollY > 0){
+        document.querySelector('.header').classList.add('active')
+    }else{
+        document.querySelector('.header').classList.remove('active')
+    }
+
+    section.forEach(sec =>{
+        let top = window.scrollY
+        let offset = sec.offsetTop - 200
+        let height = sec.offsetHeight
+        let id = sec.getAttribute('id')
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(link =>{
+                link.classList.remove('active')
+                document.querySelector('.header .navbar a[href*='+id+']').classList.add('active')
+            })
+        }
+    })
+}
+
 
 let swiper1 = new Swiper(".products-slider",{
     spaceBetween: 30,
